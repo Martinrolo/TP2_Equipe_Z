11c65cbcd8c217ec5c7533682d0fb04317c3afc3
Martinrolo <martinrolod@hotmail.com>
2023-11-18 19:32:32 -0500

    Ajouté plusieurs fonctions t_sortie.c

--- t_sortie.c
+++ t_sortie.c
@@ -11,28 +11,32 @@ t_sortie* t_sortie_init(int id, char* nom)
 {
 	t_sortie* sortie = (t_sortie*)malloc(sizeof(t_sortie));
 
-	entree->id = id;
-	entree->nom = (char*)calloc(NOM_ENTREE_TAILLE_MAX, sizeof(char));
+	sortie->id = id;
+	sortie->nom = (char*)calloc(NOM_ENTREE_TAILLE_MAX, sizeof(char));
 
-	return entree;
+	return sortie;
 }
 
 /***************************************************************************/
 void t_sortie_destroy(t_sortie* sortie)
 {
+	//D'abord détruire le pin d'entrée de la sortie
+	t_pin_sortie_destroy(sortie->pin);
 
+	free(sortie->nom);
+	free(sortie);
 }
 
 /***************************************************************************/
 t_pin_entree* t_sortie_get_pin(t_sortie* sortie)
 {
-
+	return sortie->pin;
 }
 
 /***************************************************************************/
 int t_sortie_relier(t_sortie* dest, char* nom_composant, const t_pin_sortie* source)
 {
-
+	t_pin_entree_relier(&dest->pin, &nom_composant, &source);
 }
 
 /***************************************************************************/
