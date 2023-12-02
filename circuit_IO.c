9f2e3016884b51f32fd8087c2e19384bf1cd081b
Martinrolo <martinrolod@hotmail.com>
2023-12-02 13:53:16 -0500

    Commencé liaisons + réglé bug d'accès au nom de composants

--- circuit_IO.c
+++ circuit_IO.c
@@ -7,7 +7,9 @@ Auteurs: Martin Rolo Dussault, Antoine St-Amour, Maxim Dmitriev
 #define _CRT_SECURE_NO_WARNINGS
 #include "circuit_IO.h"
 
-#define NB_CHAR_SERIALISER 10 //Nombre de caractères par ligne qu'on va sérialiser
+#define NB_CHAR_SERIALISER	10	//Nombre de caractères par ligne qu'on va sérialiser
+#define NB_CHAR_COMPOSANT	2	//Nombre de caractères par nom de composant
+#define CONVERT_ASCII		48	//Nombre pour faire la conversion entre int et ASCII
 
 void circuit_IO_sauvegarder(const char* nom_fichier, const t_circuit* circuit)
 {
@@ -110,14 +112,13 @@ void circuit_IO_charger(const char* chemin_acces, t_circuit* circuit)
 	fscanf(fentree, "%d", &circuit->nb_sorties);
 	fscanf(fentree, "%d", &circuit->nb_portes);
 
-	//Variables ID et Nom pour chaque composant qu'on va lire
-	int id = 0;
-	e_types_portes type = 0;
-	char* nom[2];	//Chaque nom de composant est composé de 2 caractères
-
 	//Faire la lecture de l'id et du nom et les mettre pour chaque entrée
 	for (int i = 0; i < circuit->nb_entrees; i++)
 	{
+		int id = NULL;
+		char nom_composant[NB_CHAR_COMPOSANT];
+		char* nom = _strdup(nom_composant);		//Allouer mémoire pour chaque nom
+
 		//Lire l'ID et le nom afin d'initialiser l'entrée et l'ajouter au circuit
 		fscanf(fentree, "%d%s\n", &id, nom);
 		circuit->entrees[i] = t_entree_init(id, nom);
@@ -126,6 +127,10 @@ void circuit_IO_charger(const char* chemin_acces, t_circuit* circuit)
 	//Faire la lecture de l'id et du nom et les mettre pour chaque sortie
 	for (int i = 0; i < circuit->nb_sorties; i++)
 	{
+		int id = NULL;
+		char nom_composant[NB_CHAR_COMPOSANT];
+		char* nom = _strdup(nom_composant);		//Allouer mémoire pour chaque nom
+
 		//Lire l'ID et le nom afin d'initialiser la sortie et l'ajouter au circuit
 		fscanf(fentree, "%d%s\n", &id, nom);
 		circuit->sorties[i] = t_sortie_init(id, nom);
@@ -134,8 +139,71 @@ void circuit_IO_charger(const char* chemin_acces, t_circuit* circuit)
 	//Faire la lecture de l'id et du nom et les mettre pour chaque porte
 	for (int i = 0; i < circuit->nb_portes; i++)
 	{
+		int id = NULL;
+		e_types_portes type = 0;
+		char nom_composant[NB_CHAR_COMPOSANT];
+		char* nom = _strdup(nom_composant);		//Allouer mémoire pour chaque nom
+
 		//Lire l'ID et le nom afin d'initialiser la porte et l'ajouter au circuit
 		fscanf(fentree, "%d%d%s\n", &id, &type, nom);
 		circuit->portes[i] = t_porte_init(id, type, nom);
 	}
+
+	//Lire les liaisons
+	while (!feof(fentree))
+	{
+		int porte_nb_entrees;
+		int porte_position;
+		int liaison_position;
+
+		char nom_composant[NB_CHAR_COMPOSANT];	//Nom du composant
+		char nom_liaison[NB_CHAR_COMPOSANT];	//Nom de la liaison
+
+		//Lire nom de l'objet-destination
+		fscanf(fentree, "%s", nom_composant);
+		printf("%c", nom_composant[0]);
+		printf("%c", nom_composant[1]);
+
+		//Si l'objet est une porte
+		if (nom_composant[0] == 'P')
+		{
+			//Le 2e caractère équivaut à la position de la porte
+			porte_position = (int)nom_composant[1] - CONVERT_ASCII;
+			porte_nb_entrees = circuit->portes[porte_position]->nb_entrees;
+
+			//Lire prochain nom(s) de liaison(s) dans le fichier, selon le nombre de pin d'entrées
+			for (int j = 0; j < porte_nb_entrees; j++)
+			{
+				fscanf(fentree, "%s", nom_liaison);
+
+				//Pin de sortie de la source de la liaison
+				t_pin_sortie* source = NULL;
+
+				//Si c'est une entrée, on va la chercher et prendre son pin de sortie
+				if (nom_liaison[0] == 'E')
+				{
+					liaison_position = nom_liaison[1] - CONVERT_ASCII;
+
+					//Chercher le pin de sortie de l'entrée source selon la position
+					t_pin_sortie* source = t_entree_get_pin(circuit->entrees[liaison_position]);
+				}
+
+				//Sinon, on va chercher la porte source et son pin de sortie
+				else
+				{
+					liaison_position = nom_liaison[1] - CONVERT_ASCII;
+
+					//Chercher le pin de sortie de l'entrée source selon la position
+					t_pin_sortie* source = t_porte_get_pin_sortie(circuit->portes[liaison_position]);
+				}
+
+				//Faire la liaison
+				t_porte_relier(circuit->portes[porte_position], j, nom_liaison, source);
+			}
+		}
+
+
+	}
+
+	fclose(fentree);
 }
